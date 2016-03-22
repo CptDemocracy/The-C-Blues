62ad1471cc4a0969fb2dba2ea6d90924f98697dc
CptDemocracy <profit234@gmail.com>
2016-03-22 21:16:22 +0200

    Tinkering with va_list's
    Do not read without a HAZMAT suit and type safety helmets on!

---
+++ March-20th-2016/Temp.c
@@ -0,0 +1,51 @@
+#include "Temp.h"
+
+#include <stdarg.h>
+#include <stdio.h>
+
+void fn1(size_t argCount, ...) {
+	va_list vl;
+	va_start(vl, argCount);
+	for (size_t i = 0; i < argCount; ++i) {
+		int arg = va_arg(vl, int);
+		printf("%d ", arg);
+	}
+	printf("\n");
+	va_end(vl);
+}
+
+char fn2(size_t argCount, ...) {
+	va_list vl;
+	va_start(vl, argCount);
+	for (size_t i = 0; i < argCount; ++i) {
+		char arg = va_arg(vl, char);
+		printf("%c ", arg);
+	}
+	printf("\n");
+	va_end(vl);
+	return '\0';
+}
+
+int fn3(size_t argCount, ...) {
+	va_list vl;
+	va_start(vl, argCount);
+	for (size_t i = 0; i < argCount; ++i) {
+		int arg = va_arg(vl, int);
+		printf("%d ", arg);
+	}
+	printf("\n");
+	va_end(vl);
+	return 0;
+}
+
+float fn4(size_t argCount, ...) {
+	va_list vl;
+	va_start(vl, argCount);
+	for (size_t i = 0; i < argCount; ++i) {
+		double arg = va_arg(vl, double);
+		printf("%f ", arg);
+	}
+	printf("\n");
+	va_end(vl);
+	return 0.0F;
+}
