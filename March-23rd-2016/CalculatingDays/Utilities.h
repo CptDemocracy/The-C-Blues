5717dbfccbad4cd40c12cb8a52ae4d34943f4fe2
CptDemocracy <profit234@gmail.com>
2016-03-23 04:58:49 +0200

    Create Utilities.h

---
+++ March-23rd-2016/CalculatingDays/Utilities.h
@@ -0,0 +1,19 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+#define JAN	0
+#define FEB	1
+#define	MAR	2
+#define	APR	3
+#define	MAY	4
+#define JUN	5
+#define JUL	6
+#define AUG	7
+#define	SEP	8
+#define OCT	9
+#define NOV	10
+#define DEC	11
+
+int CalculateDaysBetween(const struct tm* date1, const struct tm* date2);
+
+#endif /* UTILITIES_H */
