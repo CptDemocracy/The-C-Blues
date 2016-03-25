f8fce19bd06645dee18fb5b918e1d7c6d929881a
CptDemocracy <profit234@gmail.com>
2016-03-25 02:28:45 +0200

    Create rot.c

---
+++ March-24th-2016/rotations/rot.c
@@ -0,0 +1,35 @@
+#include <stdint.h>
+#include "rot.h"
+
+
+uint64_t ROTR64U(uint64_t uword64) {
+	return (uword64 >> 1ULL) | (uword64 << 63ULL);
+}
+
+uint64_t ROTL64U(uint64_t uword64) {
+	return (uword64 << 1ULL) | (uword64 >> 63ULL);
+}
+
+uint32_t ROTR32U(uint32_t uword32) {
+	return (uword32 >> 1U) | (uword32 << 31U); 
+}
+
+uint32_t ROTL32U(uint32_t uword32) {
+	return (uword32 << 1U) | (uword32 >> 31U);
+}
+
+uint16_t ROTR16U(uint16_t uword16) {
+	return (uword16 >> 1U) | (uword16 << 15U);
+}
+
+uint16_t ROTL16U(uint16_t uword16) {
+	return (uword16 << 1U) | (uword16 >> 15U);
+}
+
+uint8_t ROTR8U(uint8_t uword8) {
+	return (uword8 >> 1U) | (uword8 << 7U);
+}
+
+uint8_t ROTL8U(uint8_t uword8) {
+	return (uword8 << 1U) | (uword8 >> 7U);
+}
