f4fd10420469cabfdf1403bfc73613ee666ed5da
CptDemocracy <profit234@gmail.com>
2016-03-25 02:28:25 +0200

    implementation of x8086-like rots in C

---
+++ March-24th-2016/rotations/rot.h
@@ -0,0 +1,21 @@
+#ifndef ROT_H
+#define ROT_H
+
+
+uint64_t ROTR64U(uint64_t uword64);
+
+uint64_t ROTL64U(uint64_t uword64);
+
+uint32_t ROTR32U(uint32_t uword32);
+
+uint32_t ROTL32U(uint32_t uword32);
+
+uint16_t ROTR16U(uint16_t uword16);
+
+uint16_t ROTL16U(uint16_t uword16);
+
+uint8_t ROTR8U(uint8_t uword8);
+
+uint8_t ROTL8U(uint8_t uword8);
+
+#endif /* ROT_H */
