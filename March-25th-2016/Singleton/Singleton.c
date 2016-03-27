567869ccd65be8a91bb9ea295167ff1cda160047
CptDemocracy <profit234@gmail.com>
2016-03-28 00:52:36 +0300

    fixed formatting

--- March-25th-2016/Singleton/Singleton.c
+++ March-25th-2016/Singleton/Singleton.c
@@ -11,18 +11,18 @@ static struct Singleton obj = { 0 };
 static void SingletonDispose(void);
 
 void SingletonInitialize(void) {
-	// setup automatic deallocation routine on program exit
-	// so that users won't have to deallocate our Singleton
-	// explicitly
-	obj.ptr = calloc(1, sizeof(int));
-	atexit(SingletonDispose);
+    // setup automatic deallocation routine on program exit
+    // so that users won't have to deallocate our Singleton
+    // explicitly
+    obj.ptr = calloc(1, sizeof(int));
+    atexit(SingletonDispose);
 }
 
 static void SingletonDispose(void) {
-	free(obj.ptr);
-	printf("Singleton has been disposed of.\n");
+    free(obj.ptr);
+    printf("Singleton has been disposed of.\n");
 }
 
 struct Singleton* SingletonGet(void) {
-	return &obj;
+    return &obj;
 }
