a74b9d3ed3b2c484dfe5cd3d118e7e99ac10f84a
CptDemocracy <profit234@gmail.com>
2016-03-28 00:46:16 +0300

    fixed formatting

--- March-26th-2016/K-and-R/removeDuplicateBlanks.c
+++ March-26th-2016/K-and-R/removeDuplicateBlanks.c
@@ -9,40 +9,40 @@
 
 int main(int argc, const char** argv) {
 
-	char currc, prevc;
+    char currc, prevc;
 
-	while (1) 
-	{
-		printf(">>> ");
-	
-		currc = getchar();
-		prevc = 0;
-		while (currc != '\n') {
+    while (1) 
+    {
+        printf(">>> ");
+    
+        currc = getchar();
+        prevc = 0;
+        while (currc != '\n') {
 
-			// I agree, this test is too "hackish", however,
-			// what it simply does, is it checks whether IF
-			// NOT current char is whitespace AND the previous
-			// char was whitespace as well, if yes, we print
-			// the character to the output, if not, we ignore
-			// it
-			//
-			// !(prevc ^ currc) can be simplified as 
-			// (prevc ^ currc) == 0, if it's 0, then prev char
-			// was the same char as the current one (in our case,
-			// both were whitespace). Why? Because XOR is similar
-			// to OR, except that if we XOR two set bits, it will
-			// return 0. If two values are the same, XORing them
-			// will produce a zero
+            // I agree, this test is too "hackish", however,
+            // what it simply does, is it checks whether IF
+            // NOT current char is whitespace AND the previous
+            // char was whitespace as well, if yes, we print
+            // the character to the output, if not, we ignore
+            // it
+            //
+            // !(prevc ^ currc) can be simplified as 
+            // (prevc ^ currc) == 0, if it's 0, then prev char
+            // was the same char as the current one (in our case,
+            // both were whitespace). Why? Because XOR is similar
+            // to OR, except that if we XOR two set bits, it will
+            // return 0. If two values are the same, XORing them
+            // will produce a zero
 
-			if (!(isblank(currc) && !(prevc ^ currc))) {
-				putchar(currc);
-			}
-			prevc = currc;
-			currc = getchar();
-		}
-		
-		putchar('\n');
-	}
+            if (!(isblank(currc) && !(prevc ^ currc))) {
+                putchar(currc);
+            }
+            prevc = currc;
+            currc = getchar();
+        }
+        
+        putchar('\n');
+    }
 
-	return 0;
+    return 0;
 }
