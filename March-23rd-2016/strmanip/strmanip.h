1fb2119e85cdbcfa2cf1dd5660cd305f0d85f065
CptDemocracy <profit234@gmail.com>
2016-03-28 00:57:50 +0300

    fixed formatting

--- March-23rd-2016/strmanip/strmanip.h
+++ March-23rd-2016/strmanip/strmanip.h
@@ -2,47 +2,47 @@
 #define STRMANIP_H
 
 int KMP(const char* source, 
-	int start, 
-	int sourceSize, 
-	const char* pattern, 
-	int patternSize);
+    int start, 
+    int sourceSize, 
+    const char* pattern, 
+    int patternSize);
 
 int* KMPCalculateDFA(const char* pattern, 
-	int patternLen, 
-	int* OutResultDFA);
+    int patternLen, 
+    int* OutResultDFA);
 
 int CountWords(const char* text);
 
 int Substring(const char* target, 
-	int start, 
-	int count, 
-	char* OutResult);
+    int start, 
+    int count, 
+    char* OutResult);
 
 int IsSubstringOf(const char* target, const char* substring);
 
 int FindString(const char* target, const char* substring);
 
 int RemoveString(char* text, 
-	int start, 
-	int count);
+    int start, 
+    int count);
 
 int InsertString(const char* source, 
-	const char* insert, 
-	int index, 
-	char* OutResult);
+    const char* insert, 
+    int index, 
+    char* OutResult);
 
 int FindSubstring(const char* source, 
-	const char* substring, 
-	int start);
+    const char* substring, 
+    int start);
 
 int ReplaceFirstSubstring(const char* source,
-	const char* target,
-	const char* replac,
-	char* OutResult);
+    const char* target,
+    const char* replac,
+    char* OutResult);
 
 int ReplaceAllSubstrings(const char* source, 
-	const char* target, 
-	const char* replac, 
-	char* OutResult);
+    const char* target, 
+    const char* replac, 
+    char* OutResult);
 
 #endif /* STRMANIP_H */
