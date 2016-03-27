50acfd45d1f31dc7bc81944cf1dde5897036f1e5
CptDemocracy <profit234@gmail.com>
2016-03-28 00:51:29 +0300

    fixed formatting

--- March-25th-2016/bitops.h
+++ March-25th-2016/bitops.h
@@ -8,12 +8,12 @@
 
 // count the number of bits in an integer
 // implemented as a macro
-#define BIT_SIZE(type, resultVarName)			\
-	do {						\
-			type x = (type)1;		\
-			resultVarName = 1;		\
-			while(x <<= 1) ++resultVarName; \
-	} while (0)
+#define BIT_SIZE(type, resultVarName)          	\
+    do {                        		\
+            type x = (type)1;        		\
+            resultVarName = 1;        		\
+            while(x <<= 1) ++resultVarName; 	\
+    } while (0)
 
 // show us if given type is signed
 // implemented as a macro
@@ -24,27 +24,27 @@
 // as a macro
 // NOTE, signed or not, this implementation works both for logical right shifts
 // and arithmetic right shifts
-#define ROTL(type, value, resultVarName)		\
-	do {						\
-		int bcount = 0;				\
-		BIT_SIZE((type), (bcount));		\
-		resultVarName = ((((value) >> ((bcount) - 1)) & 1) | ((value) << 1)); \
-	} while (0)
+#define ROTL(type, value, resultVarName)        \
+    do {                        		\
+        int bcount = 0;                		\
+        BIT_SIZE((type), (bcount));        	\
+        resultVarName = ((((value) >> ((bcount) - 1)) & 1) | ((value) << 1)); \
+    } while (0)
 
 // ditto, ROTR
 // NOTE, signed or not, this implementation works both for logical right shifts
 // and arithmetic right shifts
-#define ROTR(type, value, resultVarName)		\
-	do {						\
-		int bcount = 0;				\
-		BIT_SIZE((type), (bcount));		\
-		resultVarName = (((value) >> 1) & ~((1 << ((bcount) - 1)))) | ((value) << ((bcount) - 1)); \
-	} while (0)
+#define ROTR(type, value, resultVarName)        \
+    do {                        		\
+        int bcount = 0;                		\
+        BIT_SIZE((type), (bcount));        	\
+        resultVarName = (((value) >> 1) & ~((1 << ((bcount) - 1)))) | ((value) << ((bcount) - 1)); \
+    } while (0)
 
 // returns true if all ON bits in the bitPattern
 // are ON in the value as well
 #define HAS_ALL_BITS_ON(value, bitPattern) \
-	(((value) & (bitPattern)) == (value))
+    (((value) & (bitPattern)) == (value))
 
 // returns true if the bit at position bitPos
 // is ON in the value
@@ -52,7 +52,7 @@
 // i.e. first bit position is at 0, second at 1,
 // and so on
 #define IS_BIT_ON(value, bitPos) \
-	((value) & (1 << (bitPos)))
+    ((value) & (1 << (bitPos)))
 
 // extracts bitCount bits starting at bitPos in the 
 // value, returns the bits with the first bit that
@@ -67,7 +67,7 @@
 //
 // implemented as a macro
 #define EXTRACT_BITS(value, bitPos, bitCount) \
-	(((value) & (((1 << (bitCount)) - 1) << (bitPos))) >> (bitPos))
+    (((value) & (((1 << (bitCount)) - 1) << (bitPos))) >> (bitPos))
 
 // takes bits from bitPattern starting at position bitPos
 // in the number of bitCount, and sets these bits in the
@@ -75,31 +75,31 @@
 // 
 // implemented as a macro
 #define SET_BITS(value, bitPattern, bitPos, bitCount) \
-	((value) | ((bitPattern) & (((1 << (bitCount)) - 1) << (bitPos))))
+    ((value) | ((bitPattern) & (((1 << (bitCount)) - 1) << (bitPos))))
 
 // return the bit position of a bit pattern in 
 // value or -1 if no such bit pattern is present
 // in the value
 static inline int FindBitPattern(int value, int bitPattern) {
-	
-	const int NOT_FOUND = -1;
+    
+    const int NOT_FOUND = -1;
 
-	if (!bitPattern && !value) {
-		return 0;
-	}
-	else if (!bitPattern) {
-		return NOT_FOUND;
-	}
+    if (!bitPattern && !value) {
+        return 0;
+    }
+    else if (!bitPattern) {
+        return NOT_FOUND;
+    }
 
-	int bitPos = 0;
-	while (value >= bitPattern) {
-		if ((value & bitPattern) == bitPattern) {
-			return bitPos;
-		}
-		value >>= 1;
-		++bitPos;
-	}
-	return NOT_FOUND;
+    int bitPos = 0;
+    while (value >= bitPattern) {
+        if ((value & bitPattern) == bitPattern) {
+            return bitPos;
+        }
+        value >>= 1;
+        ++bitPos;
+    }
+    return NOT_FOUND;
 }
 
 #endif /* BITOPS_H */
