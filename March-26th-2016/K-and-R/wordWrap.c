4f649d0dd2ff6c85adb2ce75b504c1449ed44d11
CptDemocracy <profit234@gmail.com>
2016-03-28 00:49:10 +0300

    fixed formatting

--- March-26th-2016/K-and-R/wordWrap.c
+++ March-26th-2016/K-and-R/wordWrap.c
@@ -11,75 +11,75 @@
  before the specified column.
 */
 
-#define BUFFER_INIT_CAPACITY	128
-#define COLS_PER_LINE			10
+#define BUFFER_INIT_CAPACITY    128
+#define COLS_PER_LINE            10
 
 int main(int argc, const char** argv) {
 
-	size_t bufferCapacity = BUFFER_INIT_CAPACITY;
-	char* buffer = (char*)malloc(sizeof(char) * BUFFER_INIT_CAPACITY);
-	if (!buffer) {
-		errno = ENOMEM;
-		return 1;
-	}
+    size_t bufferCapacity = BUFFER_INIT_CAPACITY;
+    char* buffer = (char*)malloc(sizeof(char) * BUFFER_INIT_CAPACITY);
+    if (!buffer) {
+        errno = ENOMEM;
+        return 1;
+    }
 
-	size_t bufferCount = 0;
+    size_t bufferCount = 0;
 
-	char c = 0;
-	size_t colCount = 0;
+    char c = 0;
+    size_t colCount = 0;
 
-	while (1)
-	{
-		bufferCount = 0;
-		colCount = 0;
+    while (1)
+    {
+        bufferCount = 0;
+        colCount = 0;
 
-		printf(">>> ");
+        printf(">>> ");
 
-		while ( (c = getchar()) != '\n') 
-		{
-			if (bufferCount >= bufferCapacity - 1)
-			{
-				// a safeguard in case someone accidentally changes bufferCapacity to 0
-				bufferCapacity = (bufferCapacity > 0) ? bufferCapacity * 2 : BUFFER_INIT_CAPACITY;
-				buffer = (char*)realloc(buffer, bufferCapacity);
-				if (!buffer) {
-					errno = ENOMEM;
-					return 1;
-				}
-			}
-			
-			// word wrapping logic
-			if (colCount >= COLS_PER_LINE) {
-				colCount = 0;
-				int i = bufferCount - 1;
-				while (1)
-				{
-					if (i <= 0) {
-						ungetc(c, stdin);
-						buffer[bufferCount] = '\n';	
-						break;
-					}
-					else if (isblank(buffer[i])) {
-						buffer[i] = '\n';
-						buffer[bufferCount] = c;
-						break;
-					}
-					--i;
-				}
-			}
-			else {
-				buffer[bufferCount] = c;				
-				++colCount;
-			}
-			++bufferCount;
-		}
-		buffer[bufferCount] = 0;
+        while ( (c = getchar()) != '\n') 
+        {
+            if (bufferCount >= bufferCapacity - 1)
+            {
+                // a safeguard in case someone accidentally changes bufferCapacity to 0
+                bufferCapacity = (bufferCapacity > 0) ? bufferCapacity * 2 : BUFFER_INIT_CAPACITY;
+                buffer = (char*)realloc(buffer, bufferCapacity);
+                if (!buffer) {
+                    errno = ENOMEM;
+                    return 1;
+                }
+            }
+            
+            // word wrapping logic
+            if (colCount >= COLS_PER_LINE) {
+                colCount = 0;
+                int i = bufferCount - 1;
+                while (1)
+                {
+                    if (i <= 0) {
+                        ungetc(c, stdin);
+                        buffer[bufferCount] = '\n';    
+                        break;
+                    }
+                    else if (isblank(buffer[i])) {
+                        buffer[i] = '\n';
+                        buffer[bufferCount] = c;
+                        break;
+                    }
+                    --i;
+                }
+            }
+            else {
+                buffer[bufferCount] = c;                
+                ++colCount;
+            }
+            ++bufferCount;
+        }
+        buffer[bufferCount] = 0;
 
-		printf("%s\n", buffer);
-	}
+        printf("%s\n", buffer);
+    }
 
-	free(buffer);
-	buffer = NULL;
+    free(buffer);
+    buffer = NULL;
 
-	return 0;
+    return 0;
 }
