eb7e48bd97f133ff7a6ead77f0c20fc6d8be5fc3
CptDemocracy <profit234@gmail.com>
2016-03-28 00:42:57 +0300

    fixed formatting

--- March-26th-2016/K-and-R/charFreqVertHistogram.c
+++ March-26th-2016/K-and-R/charFreqVertHistogram.c
@@ -18,86 +18,86 @@
 
 int main(int argc, const char** argv) {
 
-	const size_t FREQS_LEN = UCHAR_MAX + 1;
-	const char HIST_CHAR = '*';
+    const size_t FREQS_LEN = UCHAR_MAX + 1;
+    const char HIST_CHAR = '*';
 
-	size_t freqs[UCHAR_MAX + 1] = { 0 }; // initialize to zeros
-	size_t tableIndex = 0u;
-	size_t maxWLen = 0u;
+    size_t freqs[UCHAR_MAX + 1] = { 0 }; // initialize to zeros
+    size_t tableIndex = 0u;
+    size_t maxWLen = 0u;
 
-	char charBuffer[UCHAR_MAX + 1] = { 0 }; // initialize to zeros
-	// will hold only the chars found in the input to avoid
-	// traversing all UCHAR_MAX + 1 chars on each iteration,
-	// when printing a histo
-	size_t charBufferCount = 0u;
+    char charBuffer[UCHAR_MAX + 1] = { 0 }; // initialize to zeros
+    // will hold only the chars found in the input to avoid
+    // traversing all UCHAR_MAX + 1 chars on each iteration,
+    // when printing a histo
+    size_t charBufferCount = 0u;
 
-	char c = 0;
+    char c = 0;
 
-	while (1)
-	{
-		charBufferCount = 0u;
-		memset(freqs, 0, (UCHAR_MAX + 1) * sizeof(size_t));
-		memset(charBuffer, 0, (UCHAR_MAX + 1) * sizeof(char));
+    while (1)
+    {
+        charBufferCount = 0u;
+        memset(freqs, 0, (UCHAR_MAX + 1) * sizeof(size_t));
+        memset(charBuffer, 0, (UCHAR_MAX + 1) * sizeof(char));
 
-		printf(">>> ");
+        printf(">>> ");
 
-		while ( (c = getchar()) != '\n') {
-			if (!isspace(c)) {
-				tableIndex = (size_t)c;
+        while ( (c = getchar()) != '\n') {
+            if (!isspace(c)) {
+                tableIndex = (size_t)c;
 
-				// rather than corrupt memory, fail
-				if (tableIndex > FREQS_LEN) {
-					fprintf(stderr, "Unsupported character type.\n");
-					errno = ERANGE;
-					return 1;
-				}
+                // rather than corrupt memory, fail
+                if (tableIndex > FREQS_LEN) {
+                    fprintf(stderr, "Unsupported character type.\n");
+                    errno = ERANGE;
+                    return 1;
+                }
 
-				freqs[tableIndex] += 1u;
+                freqs[tableIndex] += 1u;
 
-				if (freqs[tableIndex] > maxWLen) {
-					maxWLen = freqs[tableIndex];
-				}
-			}
-		}
+                if (freqs[tableIndex] > maxWLen) {
+                    maxWLen = freqs[tableIndex];
+                }
+            }
+        }
 
-		// find the used chars and copy them to the charBuffer
-		for (size_t i = 0u; i < UCHAR_MAX + 1; ++i) {
-			if (freqs[i] > 0u) {
-				charBuffer[charBufferCount] = (char)i;
-				++charBufferCount;
-			}
-		}
+        // find the used chars and copy them to the charBuffer
+        for (size_t i = 0u; i < UCHAR_MAX + 1; ++i) {
+            if (freqs[i] > 0u) {
+                charBuffer[charBufferCount] = (char)i;
+                ++charBufferCount;
+            }
+        }
 
-		while (maxWLen > 0u) {
-			for (size_t i = 0u; i < charBufferCount; ++i) {
-				c = charBuffer[i];
-				tableIndex = (size_t)c;
-				
-				// no need to check for out of range here, since we 
-				// checked that when we collected input, however, to 
-				// avoid any unnecessary surprises, we double-check
-				if (tableIndex > FREQS_LEN) {
-					fprintf(stderr, "Unsupported character type.\n");
-					errno = ERANGE;
-					return 1;
-				}
+        while (maxWLen > 0u) {
+            for (size_t i = 0u; i < charBufferCount; ++i) {
+                c = charBuffer[i];
+                tableIndex = (size_t)c;
+                
+                // no need to check for out of range here, since we 
+                // checked that when we collected input, however, to 
+                // avoid any unnecessary surprises, we double-check
+                if (tableIndex > FREQS_LEN) {
+                    fprintf(stderr, "Unsupported character type.\n");
+                    errno = ERANGE;
+                    return 1;
+                }
 
-				if (freqs[tableIndex] == maxWLen) {
-					freqs[tableIndex] = freqs[tableIndex] > 0u ? freqs[tableIndex] - 1 : 0u;
-					putchar(HIST_CHAR);
-				} else {
-					putchar(' ');
-				}
-			}
-			putchar('\n');
-			--maxWLen;
-		}
+                if (freqs[tableIndex] == maxWLen) {
+                    freqs[tableIndex] = freqs[tableIndex] > 0u ? freqs[tableIndex] - 1 : 0u;
+                    putchar(HIST_CHAR);
+                } else {
+                    putchar(' ');
+                }
+            }
+            putchar('\n');
+            --maxWLen;
+        }
 
-		for (size_t i = 0u; i < charBufferCount; ++i) {
-			putchar(charBuffer[i]);
-		}
-		putchar('\n');
-	}
+        for (size_t i = 0u; i < charBufferCount; ++i) {
+            putchar(charBuffer[i]);
+        }
+        putchar('\n');
+    }
 
-	return 0;
+    return 0;
 }
