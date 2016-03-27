7aa77df5b531043a40afd3817178c6eec03ed2b4
CptDemocracy <profit234@gmail.com>
2016-03-28 00:48:32 +0300

    fixed formatting

--- March-26th-2016/K-and-R/wordLenVertHistogram.c
+++ March-26th-2016/K-and-R/wordLenVertHistogram.c
@@ -12,85 +12,85 @@
 
 int main(int argc, const char** argv) {
 
-	const char HIST_CHAR = '*';
-	const char EXIT_CHAR = EOF;
+    const char HIST_CHAR = '*';
+    const char EXIT_CHAR = EOF;
 
-	char c = 0;
+    char c = 0;
 
-	size_t  pWLensCapacity = 4u;
-	size_t* pWLens = (size_t*)calloc(pWLensCapacity, sizeof(size_t));
-	if (!pWLens) {
-		errno = ENOMEM;
-		return 1;
-	}
+    size_t  pWLensCapacity = 4u;
+    size_t* pWLens = (size_t*)calloc(pWLensCapacity, sizeof(size_t));
+    if (!pWLens) {
+        errno = ENOMEM;
+        return 1;
+    }
 
-	size_t pWLensCount = 0u;
+    size_t pWLensCount = 0u;
 
-	size_t wlen = 0u;
-	size_t maxlen = 0u;
-	
-	int canExit = 0;
+    size_t wlen = 0u;
+    size_t maxlen = 0u;
+    
+    int canExit = 0;
 
-	while (!canExit)
-	{
-		// print histo
-		while (maxlen > 0u) {
-			for (size_t i = 0u; i < pWLensCount; ++i) {
-				if (pWLens[i] == maxlen) {
-					pWLens[i] = pWLens[i] > 0 ? --pWLens[i] : 0;
-					putchar(HIST_CHAR);
-				} else {
-					putchar(' ');
-				}
-			}
-			putchar('\n');
-			--maxlen;
-		}
+    while (!canExit)
+    {
+        // print histo
+        while (maxlen > 0u) {
+            for (size_t i = 0u; i < pWLensCount; ++i) {
+                if (pWLens[i] == maxlen) {
+                    pWLens[i] = pWLens[i] > 0 ? --pWLens[i] : 0;
+                    putchar(HIST_CHAR);
+                } else {
+                    putchar(' ');
+                }
+            }
+            putchar('\n');
+            --maxlen;
+        }
 
-		memset(pWLens, 0, pWLensCapacity * sizeof(size_t));
-		pWLensCount = 0u;
+        memset(pWLens, 0, pWLensCapacity * sizeof(size_t));
+        pWLensCount = 0u;
 
-		printf(">>> ");
-	
-		while (1) {
-			c = getchar();
+        printf(">>> ");
+    
+        while (1) {
+            c = getchar();
 
-			if (c == EXIT_CHAR) {
-				canExit = 1;
+            if (c == EXIT_CHAR) {
+                canExit = 1;
 
-				// flush redundant input in the buffer if any
-				while (getchar() != '\n') continue;
+                // flush redundant input in the buffer if any
+                while (getchar() != '\n') continue;
 
-				break;
-			}
-			else if (isspace(c) || c == '\n') {
-				if (pWLensCount >= pWLensCapacity) {
-					pWLensCapacity *= 2u;
-					size_t* newPWLens = (size_t*)calloc(pWLensCapacity, sizeof(size_t));
-					if (!newPWLens) {
-						errno = ENOMEM;
-						return 1;
-					}
-					memcpy(newPWLens, pWLens, pWLensCount * sizeof(size_t));
-					free(pWLens);
-					pWLens = newPWLens;
-				}
-				pWLens[pWLensCount] = wlen;
-				++pWLensCount;
+                break;
+            }
+            else if (isspace(c) || c == '\n') {
+                if (pWLensCount >= pWLensCapacity) {
+                    pWLensCapacity *= 2u;
+                    size_t* newPWLens = (size_t*)calloc(pWLensCapacity, sizeof(size_t));
+                    if (!newPWLens) {
+                        errno = ENOMEM;
+                        return 1;
+                    }
+                    memcpy(newPWLens, pWLens, pWLensCount * sizeof(size_t));
+                    free(pWLens);
+                    pWLens = newPWLens;
+                }
+                pWLens[pWLensCount] = wlen;
+                ++pWLensCount;
 
-				if (wlen > maxlen) {
-					maxlen = wlen;
-				}
-				wlen = 0u;
-				
-				if (c == '\n') break;
+                if (wlen > maxlen) {
+                    maxlen = wlen;
+                }
+                wlen = 0u;
+                
+                if (c == '\n') break;
 
-			} else {
-				++wlen;
-			}
-		}
-	}
-	free(pWLens); 
-	pWLens = NULL;
-	return 0;
+            } else {
+                ++wlen;
+            }
+        }
+    }
+    free(pWLens); 
+    pWLens = NULL;
+    return 0;
 }
