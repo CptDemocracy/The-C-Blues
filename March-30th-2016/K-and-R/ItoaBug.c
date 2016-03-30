d72586218f36ba59cd5c3be2f1f7767f51d0fffa
CptDemocracy <profit234@gmail.com>
2016-03-30 06:28:55 +0300

    corrected the while test

--- March-30th-2016/K-and-R/ItoaBug.c
+++ March-30th-2016/K-and-R/ItoaBug.c
@@ -62,7 +62,7 @@ void Itoa(int n, char s[])
         int digit = (sign < 0) ? -(n % 10) + '0' : n % 10 + '0';
         s[i++] = digit; /* get next digit */    
     } while (!(sign >= 0 && (n /= 10) < 0) &&
-            !(sign < 0 && (n /= 10) > 0));
+            !(sign < 0 && (n /= 10) >= 0));
     if (sign < 0)
         s[i++] = '-';
     s[i] = '\0';
