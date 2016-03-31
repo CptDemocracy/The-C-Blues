77727360fbf2f1d86b02eee9418bcc2953e6a936
CptDemocracy <profit234@gmail.com>
2016-03-31 22:33:09 +0300

    Create detab.c

---
+++ March-31st-2016/K-and-R/detab.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/*K&R C (Dennis M. Ritchie & Brian W. Kernighan)
+ Exercise 5-11. Modify the program entab and detab (written as exercises in 
+ Chapter 1) to accept a list of tab stops as arguments. Use the default tab 
+ settings if there are no arguments.
+*/
+
+int main(int argc, const char** argv) {
+
+    int arg = 1;
+    int tabcount = 1;
+    int count = 0;
+    char c = 0;
+    
+    while (1) {
+        tabcount = atoi(argv[arg]);
+        tabcount = tabcount > 0 ? tabcount : 1;
+        count = 0;
+        printf(">>> ");
+        while ( (c = getchar()) != '\n') {
+            if (c == '\t') {
+                for (int i = 0; i < tabcount - count; ++i) {
+                    putchar(' ');
+                }
+            }
+            putchar(c);
+            count = (count + 1) % tabcount;
+        }
+        putchar('\n');
+        arg = (arg + 1) % argc;
+        arg = arg > 0 ? arg : 1;
+    }
+
+    return 0;
+}
