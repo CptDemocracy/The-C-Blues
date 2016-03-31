3bc53f90091517fee0c717477855cfa0aef69b8c
CptDemocracy <profit234@gmail.com>
2016-03-31 15:58:52 +0300

    Create strncat.c

---
+++ March-31st-2016/K-and-R/strncat.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+/*K&R C (Dennis M. Ritchie & Brian W. Kernighan)
+ Exercise 5-3. Write a pointer version of the function strcat that we 
+ showed in Chapter 2: strcat(s,t) copies the string t to the end of s.
+*/
+
+#define BUFFER_SIZE 256
+
+char* Strcat(char* dest, const char* src) {
+    if (!dest || !src) {
+        return NULL;
+    }
+    while (*dest) {
+        ++dest;
+    }
+    while (1) {
+        *dest = *src;
+        if (!*src) {
+            break;
+        }
+        ++dest, ++src;
+    }
+    return dest;
+}
+
+char* Strncat(char* dest, const char* src, size_t count) {
+    if (!dest || !src) {
+        return NULL;
+    }
+    while (*dest) {
+        ++dest;
+    }
+    size_t i = 0;
+    while (*src != 0 && i < count) {
+        *dest = *src;
+        ++dest, ++src, ++i;
+    }
+    *dest = 0;
+    return dest;
+}
+
+int main(void) {    
+
+    char buffer[BUFFER_SIZE] = { 0 };
+
+    strcpy(buffer, "These ");
+    Strcat(buffer, "strings ");
+    Strcat(buffer, "are ");
+    Strcat(buffer, "concatenated");
+    printf("%s.\n", buffer);
+
+    getchar();
+    return 0;
+}
