86a113fc8983d631071ddb25a4aaaaa1075a884f
CptDemocracy <profit234@gmail.com>
2016-03-31 13:22:56 +0300

    Update ungets.c

--- March-31st-2016/K-and-R/ungets.c
+++ March-31st-2016/K-and-R/ungets.c
@@ -5,29 +5,29 @@
  Exercise 4-7. Write a routine ungets(s) that will push back an entire string onto the input.
 */
 
-#define BUFFER_SIZE	256
+#define BUFFER_SIZE 256
 
 const char* ungets(const char* s, FILE* stream) {
-	if (!s) return NULL;
-	for (size_t i = strlen(s); i > 0; --i) {
-		ungetc(s[i - 1], stream);
-	}	
-	return s;
+    if (!s) return NULL;
+    for (size_t i = strlen(s); i > 0; --i) {
+        ungetc(s[i - 1], stream);
+    }    
+    return s;
 }
 
 int main(void) {
 
-	char buffer[BUFFER_SIZE] = { 0 };
+    char buffer[BUFFER_SIZE] = { 0 };
 
-	char c = 0;
-	char d = 0;
+    char c = 0;
+    char d = 0;
 
-	while (1)
-	{
-		fgets(buffer, BUFFER_SIZE, stdin);
-		printf("%s\n", buffer);
-		ungets(buffer, stdin);
-	}
+    while (1)
+    {
+        fgets(buffer, BUFFER_SIZE, stdin);
+        printf("%s\n", buffer);
+        ungets(buffer, stdin);
+    }
 
     getchar();
     return 0;
