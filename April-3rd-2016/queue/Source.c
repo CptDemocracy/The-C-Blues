eb707ef1fb9c3665e5ede9df2948fe5811cd637d
CptDemocracy <profit234@gmail.com>
2016-04-03 10:20:17 +0300

    Create Source.c

---
+++ April-3rd-2016/queue/Source.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "Queue.h"
+
+/* a driver program to test Queue */
+
+int main(void) {
+
+    struct Queue queue = { 0 };
+    QueueNew(&queue, sizeof(double));
+    
+    double lz = 0.0;
+    
+    for (int i = 0; i < 5; ++i) {
+        QueueEnqueue(&queue, &lz);
+        printf("Enqueued: %lf\n", lz);
+        ++lz;
+    }
+
+    QueueDequeue(&queue, &lz);
+    QueueDequeue(&queue, &lz);
+
+    while (QueueGetCount(&queue) > 0) {
+        QueueDequeue(&queue, &lz);
+        printf("Dequeued: %lf\n", lz);
+    }
+
+    QueueDispose(&queue);
+    getchar();
+    return 0;
+}
