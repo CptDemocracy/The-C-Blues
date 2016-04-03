92c0aadb54e10ce9d9fa08f99e32aae4a2ea04b0
CptDemocracy <profit234@gmail.com>
2016-04-03 10:50:54 +0300

    Update Vector.h

--- March-20th-2016/Vector.h
+++ March-20th-2016/Vector.h
@@ -14,7 +14,7 @@ struct Vector {
 
 int VectorNew(struct Vector*, size_t itemSize, int (*DisposeFn)(void*));
 int VectorDispose(struct Vector*);
-int VectorAdd(struct Vector*, void* item);
+int VectorAdd(struct Vector*, const void* item);
 size_t VectorGetCount(struct Vector*);
 int VectorGet(struct Vector*, size_t index, void* OutResult);
 int VectorSet(struct Vector*, size_t index, void* item);
