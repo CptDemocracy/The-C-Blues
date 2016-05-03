9235c958bb1abf3282c86f7e4d9a029c364d8b77
Efim Bogdanovsky <profit234@gmail.com>
2016-05-03 19:36:29 +0300

    Create Source.c

---
+++ May-3rd-2016/C-Modern-Approach/Chapter-4/Project-2/Source.c
@@ -0,0 +1,82 @@
+/*
+K.N.King "C Programming. A Modern Approach."
+Programming project 2 p.71
+
+Extend the program in Programming Project 1 (p.71) to handle 
+three-digit numbers.
+
+*/
+
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE            16
+#define INVALID_INPUT_MSG    "Input invalid. Please try again."
+
+#define DEBUG
+#undef    DEBUG
+
+int StringReverse(char *s, int first, int last);
+
+int main(void)
+{
+    char buffer[BUFFER_SIZE] = { 0 };
+    const int DIGIT_COUNT = 3;
+
+#ifdef DEBUG
+    assert(DIGIT_COUNT < BUFFER_SIZE);
+#endif
+    int isInputValid = 0;
+    
+    printf("Please enter a %d-digit number: ", DIGIT_COUNT);
+    while (!isInputValid)
+    {
+        isInputValid = 1;
+
+        int i = 0;
+        char c = '\0';
+        while ((c = getchar()) != '\n' && c != EOF) 
+        {
+            if (isdigit(c) && i < DIGIT_COUNT) {
+                buffer[i] = c;
+                ++i;
+            } else {
+                // we cannot break out on the spot
+                // because we still need to discard
+                // illegal input
+                isInputValid = 0;    
+            }
+        }
+        buffer[i] = '\0';
+        if (i == 0) {
+            isInputValid = 0;
+        }
+        if (!isInputValid) {
+            puts(INVALID_INPUT_MSG);
+        }
+    }
+    StringReverse(buffer, 0, strlen(buffer) - 1);
+    printf("The reversal is: %s\n", buffer);
+    getchar();
+    return EXIT_SUCCESS;
+}
+
+int StringReverse(char *s, int first, int last) {
+    if (!s || first < 0 || last < first) {
+        return 1;
+    }
+    char temp = '\0';
+    while (first < last) 
+    {
+        temp     = s[first];
+        s[first] = s[last];
+        s[last]  = temp;
+
+        ++first; 
+        --last;
+    }
+    return 0;
+}
