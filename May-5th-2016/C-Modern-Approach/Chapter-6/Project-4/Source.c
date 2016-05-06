7c5235bc41b246617ba19f92e3bc21c26c47b64b
Efim Bogdanovsky <profit234@gmail.com>
2016-05-06 04:10:37 +0300

    Update Source.c

--- May-5th-2016/C-Modern-Approach/Chapter-6/Project-4/Source.c
+++ May-5th-2016/C-Modern-Approach/Chapter-6/Project-4/Source.c
@@ -49,25 +49,26 @@ int main(void)
             fprintf(stderr, "The value of trade cannot be negative. Please try again.\n");
             continue;
         }
-
-        if (value < 2500.00f) {
-            commission = 30.00f + .017f * value;
-        } 
-        else if (value < 6250.00f) {
-            commission = 56.00f + .0066f * value;
-        }
-        else if (value < 20000.00f) {
-            commission = 76.00f + .0034f * value;
-        }
-        else if (value < 50000.00f) {
-            commission = 155.00f + .0011f * value;
-        } 
         else {
-            commission = 255.00f + .0009f * value;
-        }
-        if (commission < 39.00f) commission = 39.00f;
+            if (value < 2500.00f) {
+                commission = 30.00f + .017f * value;
+            } 
+            else if (value < 6250.00f) {
+                commission = 56.00f + .0066f * value;
+            }
+            else if (value < 20000.00f) {
+                commission = 76.00f + .0034f * value;
+            }
+            else if (value < 50000.00f) {
+                commission = 155.00f + .0011f * value;
+            }
+            else {
+                commission = 255.00f + .0009f * value;
+            }
+            if (commission < 39.00f) commission = 39.00f;
 
-        printf("Commission: $%.2lf\n", commission);
+            printf("Commission: $%.2lf\n", commission);
+        }
     }
     puts("Thank you for using our application!");
     getchar();
