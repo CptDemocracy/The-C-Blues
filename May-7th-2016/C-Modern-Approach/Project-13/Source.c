225037ff7ab1180c7e54624f183d60554372c637
Efim Bogdanovsky <profit234@gmail.com>
2016-05-07 22:07:19 +0300

    Update Source.c

--- May-7th-2016/C-Modern-Approach/Project-13/Source.c
+++ May-7th-2016/C-Modern-Approach/Project-13/Source.c
@@ -15,15 +15,33 @@ Average word length: 3.4"
 // No, we will make it more challenging - we will not consider 
 // a punctuation mark to be part of the word for simplicity.
 
+#define BUFFER_SIZE 256
+
 int main(void)
 {    
-    char userInput[] = "It was deja vu all over again."; // should be supplied by the user
+    char buffer[BUFFER_SIZE] = { 0 };
     double avgWordLen = 0.0;
     size_t wordLen = 0;
     size_t wordCount = 0;
 
+    printf("Please enter a sentence: ");
+
+    if (!fgets(buffer, BUFFER_SIZE, stdin)) {
+        return EXIT_FAILURE;
+    }
+    // we can detect if users entered more characters than
+    // BUFFER_SIZE by checking the last non-null character
+    // of the user input string, if it is not a new line
+    // character, users entered more data than provided for,
+    // which means we have trailing characters in the stream
+    // we should discard
+    if (buffer[strlen(buffer) - 1] != '\n') {
+        char c = '\0';
+        while ( (c = getchar()) != '\n' && c != EOF) continue;
+    }
+
     // tokenize user input
-    char *pchar = userInput;
+    char *pchar = buffer;
     while (*pchar != '\0') {
         if (isalnum(*pchar) && !isalnum(*(pchar + 1))) {
             *(pchar + 1) = '\0';
@@ -33,7 +51,7 @@ int main(void)
         }
         ++pchar;
     }
-    pchar = userInput;
+    pchar = buffer;
     for (size_t i = 0; i < wordCount; ++i) {
         // skip past non-alnum characters in 
         // the beginning of a token
