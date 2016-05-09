ec490e24f126359a7cfb2b0fafd075f02d0757f1
Efim Bogdanovsky <profit234@gmail.com>
2016-05-09 21:55:57 +0300

    Create Stack.c

---
+++ May-9th-2016/C-Modern-Approach/Chapter-10/Project-1/Stack.c
@@ -0,0 +1,163 @@
+/*
+K.N.King "C Programming. A Modern Approach."
+Programming project 1 p.239
+
+1. Modify the stack example of Section 10.2 so that it stores
+chracters instead of integers. Next, add a main function that
+asks the user to enter a series of parentheses and/or braces,
+then indicates whether or not they're properly nested:
+
+"Enter parentheses and/or braces: ((){}{()})
+Parentheses/braces are nested properly
+"
+
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "Stack.h"
+
+const static int STACK_INIT_CAPACITY = 4;
+
+static int StackEnsureCapacity(struct Stack* self, size_t minCapacity) {
+    if (!self) {
+        errno = EINVAL;
+        return 1;
+    }
+    if (minCapacity < 1) {
+        errno = EINVAL;
+        return 2;
+    }
+    if (minCapacity > self->_itemCapacity) {
+        minCapacity = minCapacity > self->_itemCapacity * 2 ? minCapacity : self->_itemCapacity * 2;
+        void* newItems = calloc(minCapacity, self->_itemSize);
+        if (!newItems) {
+            errno = ENOMEM;
+            return 3;
+        }
+        memcpy(newItems, self->_items, self->_itemCount * self->_itemSize);
+        free(self->_items);
+
+        self->_items = newItems;
+        self->_itemCapacity = minCapacity;
+    }
+
+    return 0;
+}
+
+int StackNew(struct Stack* self, size_t itemSize, void (*disposeFn)(void*)) {
+    if (!self) {
+        errno = EINVAL;
+        return 1;
+    }
+    if (itemSize < 1) {
+        errno = EINVAL;
+        return 2;
+    }
+
+    self->_items = calloc(STACK_INIT_CAPACITY, itemSize);
+    if (!self->_items) {
+        errno = ENOMEM;
+        return 3;
+    }
+    self->_itemCapacity = STACK_INIT_CAPACITY;
+    self->_itemCount = 0;
+    self->_itemSize = itemSize;
+    self->disposeFn = disposeFn;
+
+    return 0;
+}
+
+void StackDispose(struct Stack* self) {
+    if (!self) {
+        errno = EINVAL;
+        return;
+    }
+    if (self->disposeFn != NULL) {
+        void* item = NULL;
+        size_t count = self->_itemCount;
+        for (size_t i = 0U; i < count; ++i) {
+            item = (char*)self->_items + i * self->_itemSize;
+            self->disposeFn(item);
+        }
+    }
+    free(self->_items);
+    self->_items = NULL;
+}
+
+size_t StackGetCount(struct Stack* self) {
+    if (!self) {
+        errno = EINVAL;
+        return 0U;
+    }
+    return self->_itemCount;
+}
+
+int StackPeek(struct Stack* self, void* OutResult) {
+    if (!self) {
+        errno = EINVAL;
+        return 1;
+    }
+    if (!OutResult) {
+        errno = EINVAL;
+        return 2;
+    }
+    if (self->_itemCount < 1) {
+        errno = EPERM;
+        return 3;
+    }
+    void* item = NULL;
+
+    item = (char*)self->_items + self->_itemSize * (self->_itemCount - 1);
+    memcpy(OutResult, item, self->_itemSize);
+
+    return 0;
+}
+
+int StackPush(struct Stack* self, const void* item) {
+    if (!self) {
+        errno = EINVAL;
+        return 1;
+    }
+    if (!item) {
+        errno = EINVAL;
+        return 2;
+    }
+    
+    int error = 0;
+    error = StackEnsureCapacity(self, self->_itemCount + 1);
+    if (error) {
+        return error;
+    }
+    void* copyToAddr = NULL;
+    copyToAddr = (char*)self->_items + self->_itemCount * self->_itemSize;
+
+    memcpy(copyToAddr, item, self->_itemSize);
+    ++self->_itemCount;
+
+    return 0;    
+}
+
+int StackPop(struct Stack* self, void* OutResult) {
+    if (!self) {
+        errno = EINVAL;
+        return 1;
+    }
+    if (self->_itemCount < 1) {
+        errno = EPERM;
+        return 2;
+    }
+    void* popAddr = NULL;
+    popAddr = (char*)self->_items + self->_itemSize * (self->_itemCount - 1);
+
+    if (OutResult) {
+        memcpy(OutResult, popAddr, self->_itemSize);
+    } else if (self->disposeFn != NULL) {
+        self->disposeFn(popAddr);
+    }
+    --self->_itemCount;
+
+    return 0;
+}
