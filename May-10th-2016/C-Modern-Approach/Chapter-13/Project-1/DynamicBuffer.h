846b6f80490b5e053318f74f0b27b7bd31248959
Efim Bogdanovsky <profit234@gmail.com>
2016-05-10 21:22:32 +0300

    Create DynamicBuffer.h

---
+++ May-10th-2016/C-Modern-Approach/Chapter-13/Project-1/DynamicBuffer.h
@@ -0,0 +1,30 @@
+#ifndef DYNAMICBUFFER_H
+#define DYNAMICBUFFER_H
+
+struct DynamicBuffer {
+    char* _buffer;
+    size_t _size;
+    size_t _capacity;
+};
+
+int DynamicBufferGetCount(struct DynamicBuffer* self);
+
+int DynamicBufferNew(struct DynamicBuffer* self);
+
+const char* DynamicBufferFGetString(struct DynamicBuffer* self, FILE* stream);
+
+const char* DynamicBufferGetString(struct DynamicBuffer* self);
+
+char DynamicBufferFGetChar(struct DynamicBuffer* self, FILE* stream);
+
+char DynamicBufferGetChar(struct DynamicBuffer* self);
+
+int DynamicBufferClear(struct DynamicBuffer* self);
+
+int DynamicBufferPutChar(struct DynamicBuffer* self, char c);
+
+int DynamicBufferPutString(struct DynamicBuffer* self, const char* s);
+
+void DynamicBufferDispose(struct DynamicBuffer* self);
+
+#endif /* DYNAMICBUFFER_H */
